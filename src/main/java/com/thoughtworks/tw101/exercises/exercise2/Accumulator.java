45a0a7813886e0ab13fb4c7ee3bde0a73905b2c6
Austin Garrard <wootasaurus@gmail.com>
2015-12-02 19:14:59 -0600

    Completed Exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    private int value;
+
+    public Accumulator() {
+        this.value = 0;
+    }
+
+    public void increment() {
+        this.value++;
+    }
+
+    public int total() {
+        return this.value;
+    }
+
 }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
         Accumulator accumulator = new Accumulator();
 
+        for (int i = 0; i < 5; i++) {
+            accumulator.increment();
+        }
 
+        System.out.format("Total: %d\n", accumulator.total());
     }
 }
