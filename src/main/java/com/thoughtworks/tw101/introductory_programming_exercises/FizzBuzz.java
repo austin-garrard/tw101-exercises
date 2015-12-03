6182c42aa52383c780b3dfa43d6998d875b53f3d
Austin Garrard <wootasaurus@gmail.com>
2015-12-02 22:45:59 -0600

    Completed FizzBuzz Exercise

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -33,5 +33,20 @@ public class FizzBuzz {
 
     private static void fizzBuzz() {
 
+        for (int i = 1; i <= 100; i++) {
+            StringBuilder sb = new StringBuilder();
+            if (i % 3 == 0) {
+                sb.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                sb.append("Buzz");
+            }
+            if (sb.length() == 0) {
+                sb.append(i);
+            }
+
+            System.out.println(sb.toString());
+        }
+
     }
 }
