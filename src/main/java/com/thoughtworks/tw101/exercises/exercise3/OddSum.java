7799479e25081e00aa086e857c81974eaf3eac8a
Austin Garrard <wootasaurus@gmail.com>
2015-12-02 19:20:38 -0600

    Completed Exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,17 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        if (end - start <= 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        for (int i = start + 1; i < end; i++) {
+            if (i % 2 == 1) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 }
