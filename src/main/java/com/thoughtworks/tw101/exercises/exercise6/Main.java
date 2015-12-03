e26ba6fb1cc98cfa26598ebdd9b88aa9cd35d6d9
Austin Garrard <wootasaurus@gmail.com>
2015-12-02 19:53:47 -0600

    Completed Exercise 6

--- src/main/java/com/thoughtworks/tw101/exercises/exercise6/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise6/Main.java
@@ -8,8 +8,34 @@ package com.thoughtworks.tw101.exercises.exercise6;
 // * Orcs are named “Orc” and have 20 initial hitpoints
 // * Trolls are named “Troll”, have 40 hit points and only take half damage
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Monster> monsters = new ArrayList<Monster>();
+
+        //create monsters
+        System.out.println("Creating 6 monsters...");
+        for (int i = 0; i < 3; i++) {
+            monsters.add(new Orc());
+            monsters.add(new Troll());
+        }
+        System.out.println("...done!");
+
+        //damage monsters
+        System.out.println("Damaging each monster by 10...");
+        for (Monster monster : monsters) {
+            monster.takeDamage(10);
+        }
+        System.out.println("...done!");
+
+        //report monster status
+        System.out.println("Reporting monster statuses...");
+        for (Monster monster : monsters) {
+            monster.reportStatus();
+        }
+        System.out.println("...done!");
 
     }
 }
