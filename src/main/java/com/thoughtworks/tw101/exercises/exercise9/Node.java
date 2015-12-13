b1c9e57d5a34104d3f4cf454d6dc23b174c74c2b
Austin Garrard <wootasaurus@gmail.com>
2015-12-13 00:27:42 -0600

    Added javadoc comments

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 /**
  * An unbalanced binary tree that stores strings.
+ *
+ * @author Austin Garrard
+ * @version 1.0
  */
 public class Node {
     private String name;
@@ -13,6 +16,11 @@ public class Node {
     private Node left;
     private Node right;
 
+    /**
+     * Constructs a root node.
+     *
+     * @param name The name of the node
+     */
     public Node(String name) {
         this.name = name;
         this.parent = null;
@@ -20,6 +28,12 @@ public class Node {
         this.right = null;
     }
 
+    /**
+     * Constructs a child node.
+     *
+     * @param name   The name of the node
+     * @param parent Reference to the parent node
+     */
     public Node(String name, Node parent) {
         this.name = name;
         this.parent = parent;
@@ -27,6 +41,14 @@ public class Node {
         this.right = null;
     }
 
+    /**
+     * Adds a new node to the tree rooted by this node.
+     *
+     * It will be added such that an in-order traversal of the tree
+     * yields the nodes in ascending order, according to their names.
+     *
+     * @param nameOfNewNode The name of the new node
+     */
     public void add(String nameOfNewNode) {
         if (nameOfNewNode.compareTo(this.name) < 0) {
             //add to the left
@@ -45,6 +67,11 @@ public class Node {
         }
     }
 
+    /**
+     * In-order traversal of the tree rooted at this node.
+     *
+     * @return The names(in alphabetical order) of each node in the tree rooted by this node
+     */
     public List<String> names() {
         List<String> names = new ArrayList<String>();
 
